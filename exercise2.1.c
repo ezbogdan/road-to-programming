b92964180436c8128b55f41304cb8eb7dd748afb
ezbogdan <ezbogdan@yahoo.com>
2016-08-14 15:57:17 +0300

    Changed the order of variables and initialized them after declaring in exercise2-1.c

--- exercise2.1.c
+++ exercise2.1.c
@@ -5,14 +5,14 @@
 
 int main()
 {
-    int fahr, celsius;
-    int lower, upper, step;
 
-    lower = 0;      /* lower limit of temperature table */
-    upper = 300;    /* upper limit */
-    step = 20;      /* step size */
+    int lower = 0;      /* lower limit of temperature table */
+    int upper = 300;    /* upper limit */
+    int step = 20;      /* step size */
 
-    fahr = lower;
+    int fahr = lower;
+    int celsius;
+
     while (fahr <= upper) {
         celsius = 5 * (fahr-32) / 9;
         printf("%3d %6d\n", fahr, celsius);
